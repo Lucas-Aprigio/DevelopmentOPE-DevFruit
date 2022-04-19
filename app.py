751e28eeed5722547e63a686001f0bf6e1be2b52
Lucas Aprígio da Silva <78664518+Lucas-Aprigio@users.noreply.github.com>
2022-04-19 18:45:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,8 @@ def sobre():
 def endereco():
     return render_template('endereco.html')
 
-if __name__=='main':
-    app.run(debug=True,host='0.0.0.0',port=5000)
+if __name__ == '__main__':
+    port=int(os.environ.get("PORT",5000))
+    # run app in debug mode on port 5000
+    app.run(host='0.0.0.0', port=port)
 
